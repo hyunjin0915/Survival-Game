efff69a971a1e11080209ac382118b01ed3b142b
Rachel Kim <hyunjinney@naver.com>
2022-12-31 12:21:50 +0900

    committ

---
+++ Survival Game/Assets/Scripts/Gun.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    //총마다 다른 값을 가지도록 변수들을 선언함
+    public string gunName; //총의 이름 여러개의 총 구분역할
+    public float range; //총의 사정거리
+    public float accuracy; //총의 정확도
+    public float fireRate; //연사속도
+    public float reloadTime; //재장전속도
+
+    public int damage; //총의 데미지
+    
+    public int reloadBulletCount; //총알 재장전 개수
+    public int currentBulletCount; //현재 남은 총알 개수
+    public int maxBulletCount; //최대 소유 가능 총알개수
+    public int carryBulletCount; //현재 소유 총알 개수
+
+    public float retroActionForce; //반동세기
+    public float retroActionFineSightForce; //정조준시의 반동세기
+
+    public Vector3 fineSightOriginPos;
+    public Animator anim;
+    public ParticleSystem muzzleFlash;//화염등 이팩트관리
+
+    public AudioClip fire_Sound;//총마다 발사 사운드
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Survival Game/Assets/Scripts/GunController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    [SerializeField]
+    private Gun currentGun;
+
+    private float currentFireRate; //총의FireRate값을가져와서1초에1씩깎음>0이되면발사가능
+
+    private AudioSource audioSource; //선언만.껍데기만만든것
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>(); //넣어줘야함
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        GunFireRateCalc();
+        TryFire();
+    }
+
+    private void GunFireRateCalc()
+    {
+        if (currentFireRate > 0)
+            currentFireRate -= Time.deltaTime; // 1/60, update함수는1초에60프레임->1초에1씩감소하게됨
+        
+    }
+    private void TryFire()
+    {
+        if(Input.GetButton("Fire1") && currentFireRate<=0)//누르고있는동안계속발사되게
+        {
+            Fire();
+        }
+    }
+    private void Fire()
+    {
+        currentFireRate = currentGun.fireRate; //연사속도조절
+        Shoot();
+    }
+    private void Shoot()
+    {
+        PlaySE(currentGun.fire_Sound);
+        currentGun.muzzleFlash.Play();
+        Debug.Log("총알 발사함");
+    }
+    private void PlaySE(AudioClip _clip) //효과음재생
+    {
+        audioSource.clip = _clip;
+        audioSource.Play();
+    }
+}
